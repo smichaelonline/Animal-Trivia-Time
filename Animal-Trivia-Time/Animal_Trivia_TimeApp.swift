ea99d01ade533a900eea45dedf2a5fded8c8c53f
smichaelonline <stephaniemichael001@gmail.com>
2023-02-13 20:03:55 -0500

    Initial Commit

---
+++ Animal-Trivia-Time/Animal_Trivia_TimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Animal_Trivia_TimeApp.swift
+//  Animal-Trivia-Time
+//
+//  Created by Stephanie Michael on 2/13/23.
+//
+
+import SwiftUI
+
+@main
+struct Animal_Trivia_TimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
